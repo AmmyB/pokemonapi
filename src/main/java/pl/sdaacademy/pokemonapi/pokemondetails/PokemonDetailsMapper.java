a2bb8fcd1b9701de3115fe4873fccaac3e2ed592
AmmyB <amelia.90.b@gmail.com>
2023-05-20 11:53:18 +0200

    feature(paa): add mechanism to fetch pokemon details for Pokemon Academy API purposes

---
+++ src/main/java/pl/sdaacademy/pokemonapi/pokemondetails/PokemonDetailsMapper.java
@@ -0,0 +1,41 @@
+package pl.sdaacademy.pokemonapi.pokemondetails;
+
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+class PokemonDetailsMapper {
+
+    PokemonDetailsEntity toEntity(PokeApiDetailsResult pokeApiDetailsResult) {
+        String image = null;
+        if (pokeApiDetailsResult.getSprites() != null &&
+                pokeApiDetailsResult.getSprites().getOther() != null
+                && pokeApiDetailsResult.getSprites().getOther().getOfficialArtwork() != null) {
+            image = pokeApiDetailsResult.getSprites().getOther().getOfficialArtwork().getFrontDefault();
+        }
+        List<String> abilities = null;
+        if (pokeApiDetailsResult.getAbilities() != null) {
+            abilities = pokeApiDetailsResult
+                    .getAbilities()
+                    .stream()
+                    .map(item -> item.getAbility().getName())
+                    .toList();
+        }
+        List<String> types = null;
+        if (pokeApiDetailsResult.getTypes() != null) {
+            types = pokeApiDetailsResult
+                    .getTypes()
+                    .stream()
+                    .map(item -> item.getType().getName())
+                    .toList();
+        }
+        return new PokemonDetailsEntity(
+                pokeApiDetailsResult.getHeight(),
+                pokeApiDetailsResult.getWeight(),
+                image,
+                types,
+                abilities
+        );
+    }
+}
